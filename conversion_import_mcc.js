c5bfb0be4687de0fc6980cebd4c429e4a06924a6
Pavel Chernyakov <chrnkvp@gmail.com>
2016-09-28 13:13:31 +0300

    Update conversion_import_mcc.js

--- conversion_import_mcc.js
+++ conversion_import_mcc.js
@@ -1,4 +1,4 @@
-function main() {
+ function main() {
   // set desired account ID for uploading conversions 
   // and the URL to download pre-formatted CSV from
   uploadConversions(accountId = '123-456-7890',
@@ -6,34 +6,42 @@ function main() {
 }
 
 function uploadConversions(accountId, csvUrl) {
-  var targetAccountId = accountId;
-  Logger.log("Target account set to %s", targetAccountId);
-  var csvUrl = csvUrl; 
-  Logger.log("CSV source set to %s", csvUrl);
+  Logger.log("Target account set to %s", 
+             accountId);
+  Logger.log("CSV source set to %s", 
+             csvUrl);
 
+  // subsetting the accounts list to find target by ID
   var childAccount = MccApp.accounts()
-  .withIds([targetAccountId])
+  .withIds([accountId])
   .get()
   .next();
   
+  // selecting the child account to apply the changes to
   MccApp
   .select(childAccount);
   Logger.log("Selected account %s ('%s')", 
              childAccount.getCustomerId(), 
              childAccount.getName());
   
+  // fetching the CSV from the URL specified
   var blob = UrlFetchApp.fetch(csvUrl)
   .getBlob()
   .getAs(MimeType.CSV);
-  Logger.log("Downloaded CSV from %s", csvUrl);
+  Logger.log("Downloaded CSV from %s", 
+             csvUrl);
 
+  // creating the upload object
+  // specify the correct timezone here, as 
+  // conversion timestamp cannot precede the click timestamp
   var upload = AdWordsApp
   .bulkUploads()
   .newFileUpload(blob, 
                  moneyInMicros = false, 
-                 timeZone = "-0500");
+                 timeZone = "-0500"); 
   Logger.log("Successfully created upload object.");
   
+  // set the upload type and apply
   upload.forOfflineConversions();
   upload.apply();
   Logger.log("Offline conversions has been successfully applied to account %s (%s)", 
